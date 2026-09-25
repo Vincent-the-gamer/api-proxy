7950d7bd0e604f428f528b3103837abba40f3ef7
Vincent-the-gamer <erxianqiaottjt@163.com>
2026-09-25 16:05:01 +0800

    fix: config.

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,15 +1,6 @@
 // https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: 'server',
-  routeRules: {
-    '/**': {
-      cors: true,
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Request-Method': 'PUT,POST,GET,DELETE,OPTIONS',
-      },
-    },
-  },
   preset: 'netlify-edge',
   runtimeConfig: {
     compatibilityDate: '2026-09-25',
