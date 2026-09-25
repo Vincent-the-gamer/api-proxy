28a1abce8ec11048d1e14dba30223b1c554240e7
Vincent-the-gamer <erxianqiaottjt@163.com>
2026-09-25 16:07:04 +0800

    fix: middleware.

--- nitro.config.ts
+++ nitro.config.ts
@@ -1,6 +1,15 @@
 // https://nitro.unjs.io/config
 export default defineNitroConfig({
   srcDir: 'server',
+  routeRules: {
+    '/**': {
+      cors: true,
+      headers: {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Request-Method': 'PUT,POST,GET,DELETE,OPTIONS',
+      },
+    },
+  },
   preset: 'netlify-edge',
   runtimeConfig: {
     compatibilityDate: '2026-09-25',
